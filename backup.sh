7d1cc87496138c8effa8f9d78d1fe606ed11fcd9
Momokh <mohamedkhamkhami@gmail.com>
2026-04-14 14:10:37 +0000

    new repo

---
+++ backup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BACK_UP_DIR="$HOME/BACKUP/dotfiles_$(date +%Y-%m-%d)"
+mkdir -p "$BACK_UP_DIR"
+
+configs=("waybar" "Kvantum" "fastfetch" "kitty" "hypr" "rofi")
+
+dotfiles=(".bashrc" ".zshrc")
+
+echo " starting backup to $BACK_UP_DIR ..."
+
+
+for folder in "${configs[@]}"; do
+  if [ -d "$HOME/.config/$folder" ]; then
+    cp -r "$HOME/.config/$folder" "$BACK_UP_DIR"
+    echo "  BACKUP DONE SUCCESUVELY $folder"
+  else
+    echo "   NOT found"
+  fi
+done
+
+for file in "${dotfiles_[@]}"; do
+  if [ -d "$HOME/$file" ]; then
+    cp -r "$HOME/$file" "$BACK_UP_DIR"
+    echo "   BACKUP DONE SUCCESUVELY $folder"
+  else
+    echo "    NOT found"
+  fi
+done
+
+echo "────────────────────────────────────────────────────────────"
+
+echo " finished"
+
---
+++ install_all.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set_color=$(tput setaf 2) #GREEN
+reset_color=$(tput sgr0)
+
+pacman_packages=("firefox" "tree" "nvim" "vlc" "fastfetch" "kitty" "base-devel" "htop" "git")
+
+AUR_PACKAGES=("")
+
+#update system
+echo "${set_color} ───────────────────── updating system reposotories ───────────────────── ${reset_color}"
+sudo pacman -Syu --noconfirm
+
+#install the pacman packages
+echo "${set_color} ───────────────────── installing pacman packages ─────────────────────${reset_color}"
+sudo pacman -s --noconfirm --needed "${pacman_packages[@]}"
+
+#install YAY if not
+if ! command -v yay &>/dev/null; then
+  echo "${set_color} ───────────────────── yay not found installing  ───────────────────── ${reset_color}"
+  #install git if not
+  if ! command -v git &>/dev/null; then
+    echo "${set_color} ───────────────────── git not found installing  ───────────────────── ${reset_color}"
+    sudo pacman -S --noconfirm git
+  fi
+  git clone https://aur.archlinux.org/yay.git /tmp/yay
+  cd /tmp/yay && makepkg -si --noconfirm
+  cd ... || exit
+fi
+
+# --- 5. Install AUR Packages ---
+echo "${set_color} ───────────────────── Installing AUR Packages ───────────────────── ${reset_color}"
+yay -S --needed --noconfirm "${AUR_PACKAGES[@]}"
+
+echo "${set_color} ───────────────────── Setup complete! Enjoy your Arch build ───────────────────── ${reset_color}"
