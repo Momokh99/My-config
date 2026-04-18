497aeda1bc1ee9e9d48808ba7925d61cf666ce51
Momokh <mohamedkhamkhami@gmail.com>
2026-04-18 14:36:05 +0000

    upgrade

--- install_all.sh
+++ install_all.sh
@@ -3,7 +3,7 @@
 set_color=$(tput setaf 2) #GREEN
 reset_color=$(tput sgr0)
 
-pacman_packages=("firefox" "tree" "nvim" "vlc" "fastfetch" "kitty" "base-devel" "htop" "git")
+pacman_packages=("firefox" "tree" "nvim" "clang" "pyright" "rust-analyzer" "gopls" "lua-language-server" "yaml-language-server" "bash-language-server" "vlc" "fastfetch" "kitty" "base-devel" "htop" "git")
 
 AUR_PACKAGES=("")
 
@@ -13,7 +13,7 @@ sudo pacman -Syu --noconfirm
 
 #install the pacman packages
 echo "${set_color} ───────────────────── installing pacman packages ─────────────────────${reset_color}"
-sudo pacman -s --noconfirm --needed "${pacman_packages[@]}"
+sudo pacman -S --noconfirm --needed "${pacman_packages[@]}"
 
 #install YAY if not
 if ! command -v yay &>/dev/null; then
