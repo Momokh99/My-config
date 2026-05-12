38134efc98b127f2e5c8c700260dd84c15c6d027
Momokh99 <mohamedkhamekhami@gmail.com>
2026-05-12 17:34:56 +0100

    update undo kvantum theme and add alacratty

--- nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
---
-local runner = require("config.my_runner")
-vim.keymap.set("n", "<leader><CR>", runner.runCurentFile, { desc = "Run current file" })
